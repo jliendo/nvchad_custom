dddeb2646e89eb79cb93c426a637ef1966f1f940
Javier Liendo <javier@liendo.net>
2023-03-17 20:24:39 -0600

    cambia el estilo del statusline

--- chadrc.lua
+++ chadrc.lua
@@ -11,6 +11,13 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
 
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+  },
+
   tabufline = {
     enabled = false,
     lazyload = true,
