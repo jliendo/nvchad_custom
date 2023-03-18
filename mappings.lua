adffb19e6093fae049b88d2dc2a2192806341d44
Javier Liendo <javier@liendo.net>
2023-03-18 08:58:54 -0600

    agrega telescope diagnostics

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>dd"] = { "<cmd> Telescope diagnostics  <CR>", "diagnostics" },
   },
 }
 
